09a225d323d02f5215b6af92143bd2f2ae8332f8
amir ahmadi <amirahmadidev@gmail.com>
2022-03-29 01:45:06 +0430

    init

---
+++ src/main/java/TenMinWalk.java
@@ -0,0 +1,33 @@
+public class TenMinWalk {
+
+    public static boolean isValid(char[] walk){
+        int northSouth=0;
+        int eastWest=0;
+        if(checkArrayLength(walk,10)){
+            for (char c : walk) {
+                if (c == 'n') {
+                    northSouth++;
+                }
+                if (c == 's') {
+                    northSouth--;
+                }
+                if (c == 'w') {
+                    eastWest++;
+                }
+                if (c == 'e') {
+                    eastWest--;
+                }
+            }
+            return northSouth == 0 && eastWest == 0;
+        }
+        return false;
+
+    }
+
+    private static boolean checkArrayLength(char[] value,int number) {
+        return value.length == number;
+    }
+
+
+
+}
---
+++ src/test/java/TenMinWalkTest.java
@@ -0,0 +1,37 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TenMinWalkTest {
+
+
+    @Test
+    void should_return_false_if_array_is_empty() {
+        assertThat(TenMinWalk.isValid(new char[] {})).isFalse();
+    }
+    @Test
+    void should_return_false_if_array_length_less_than_10() {
+        assertThat(TenMinWalk.isValid(new char[] {'s','w','e','n'})).isFalse();
+    }
+
+    @Test
+    void should_return_false_if_array_length_more_than_10() {
+        assertThat(TenMinWalk.isValid(new char[] {'s','w','e','n','s','w','e','n','s','w','e','n'})).isFalse();
+    }
+
+    @Test
+    void should_return_false_if_the_input_was_nothing_but_directions() {
+        assertThat(TenMinWalk.isValid(new char[] {'f','f','h','k','m','n','s','w','w','w'})).isFalse();
+    }
+
+    @Test
+    void should_return_true_if_north_south_result_will_be_0() {
+        assertThat(TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'})).isTrue();
+    }
+
+    @Test
+    void should_return_false_if_the_west_east_length_array_will_be_12() {
+        assertThat(TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'})).isFalse();
+    }
+}
